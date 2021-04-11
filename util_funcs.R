1f444f2d298890ea8668497dba83996eff6e4bdf
Zakiirim <raffdicostanzo77@gmail.com>
2021-04-11 18:23:58 +0200

    Architecture updated.

--- util_funcs.R
+++ util_funcs.R
@@ -1,10 +1,26 @@
 library(igraph)
 library(dplyr)
 library(FRACTION)
+library(tidyverse)
 
 # check for aperiodicity and strong connectivity
+show_graph<- function(W){
+  G = graph_from_adjacency_matrix(W,mode = 'directed', weighted = TRUE)
+  w<-as.factor(E(G)$weight)
+  n<-length(levels(w))
+  plot(G, edge.color= w, arrow.size=0.2, curved=T)
+  
+  
+}
 
-convergence_check <- function(matrix){
+make_opinon_plot<-function(sym){
+  t<-ncol(sym)-1
+  matplot(t(sym), ylim = c(0,1.4), type = c("b"),pch=1,col = as.factor(sym[,t+1]))
+         legend("topright", legend = unique(sym[,t+1]), col=1:length(levels(as.factor(sym[,t+1]))), pch=19, bty ="n")  #plot
+  #
+}
+
+convergence_check <- function(matrix, shout = F){
   aper_flag <- F
   conect_flag <-F
   # make graph form matrix
@@ -25,7 +41,7 @@ convergence_check <- function(matrix){
   for(i in 2:nrow(nT)){
     
     if(gcd(nT$val[i],cur_g) == 1){
-      print('Aperiodicity OK')
+      if(shout) print('Aperiodicity OK')
       aper_flag <- T
       break
     } 
@@ -37,7 +53,7 @@ convergence_check <- function(matrix){
   
   #CONECTIVITY
   if (vertex_connectivity(graph,checks = T) >0){
-    print('Connectivity OK')
+    if(shout) print('Connectivity OK')
     conect_flag <-T
   }
   # OUTPUT
@@ -45,11 +61,51 @@ convergence_check <- function(matrix){
     return(TRUE)
   }
   else if (!conect_flag){
-    print('Not connected')
+    if(shout) print('Not connected')
     return(FALSE)
   }
   else if(!aper_flag){
-    print(paste('Not aperiodic, period =',as.character(cur_g)))
+    if(shout) print(paste('Not aperiodic, period =',as.character(cur_g)))
     return(FALSE)
   }
+  
+  
 }
+
+# simulating the process
+# basic model
+markov_chains_2 <-  function(M, x0, iter ){
+  sym <- matrix(0, nrow = nrow(M), ncol =  iter )
+  M_to_p_i <- M
+  sym[,1] <- x0
+  for(i in 2:iter) {
+    
+    sym[,i] <- M_to_p_i %*% x0 
+    M_to_p_i <- M %*% M_to_p_i
+  }
+  return(sym)
+}
+
+update_rule_0 <- function(x,M,...){
+  x_next <-M %*% x
+  M_next <- M
+  return(list('x_next' = x_next, 'matrix' = M_next))
+}
+
+simulate <-  function(M, x0, iter, rule, agent_names,...){
+  #sym <- matrix(0, nrow = nrow(M), ncol =  iter )
+  sym <- matrix(0, nrow = nrow(M), ncol =  iter )
+  sym[,1] <- x0
+  cur_M<-M
+  for(i in 2:iter) {
+    cur_x <- sym[,i-1]
+    sym[,i] <- rule(cur_x,cur_M,i,...)[['x_next']]
+    cur_M <- rule(cur_x,cur_M,i,...)[['matrix']]
+  }
+  sym<-cbind(sym,agent_names)
+  return(sym)
+}
+
+
+
+
